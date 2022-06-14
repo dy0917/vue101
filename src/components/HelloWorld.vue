d3291d0ce0359cd1f452c1a639acca1a0ec563e9
Kingsley Huang <huangkingsley@gmail.com>
2022-06-14 23:07:27 +0000

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <input v-model="text" />
+    </div>
+    <div>
+      {{ text }}
+    </div>
+    <div>
+      <button @click="increase">Add 1</button>
+    </div>
+    {{ amount }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      text: undefined,
+      amount: 0,
+    };
+  },
+  methods: {
+    increase() {
+      this.amount++;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
